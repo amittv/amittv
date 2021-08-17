a9d3994abb3ea26edbbb5f6a35cf14ab48bf8734
amityerva <amity@testvagrant.com>
2021-08-17 18:32:16 +0530

    update test runner

--- src/test/java/api/TestRunner.java
+++ src/test/java/api/TestRunner.java
@@ -7,11 +7,13 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features ="src/test/resources/features/ReqRes.feature" ,
-        glue = "steps",
-        tags="@test",
-        plugin = { "pretty","html:target/cucumber-reports","json:target/cucumber-reports/cucumber.json" },
-        monochrome = true
+        features ="src/test/resources/features" ,
+//        glue = "steps",
+//        tags="@test",
+//        plugin = { "pretty","html:target/cucumber-reports","json:target/cucumber-reports/cucumber.json" },
+//        plugin = {"pretty", "html:target/cucumber/report.html", "json:target/cucumber/report.json"},
+        monochrome = true,
+        publish = true
 )
 
 
